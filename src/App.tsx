6e51e8ee5b3a7b8f6369d6117d3eb5d7d71fdb4c
okido-yukinari <yukiti0525.revolution@au.com>
2024-09-03 06:28:59 +0900

    タスクの新規追加、一覧表示機能実装

--- src/App.tsx
+++ src/App.tsx
@@ -9,24 +9,52 @@ import Typography from "@mui/material/Typography";
 import DeleteIcon from "@mui/icons-material/Delete";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import { useState } from "react";
+
+interface Task {
+  id: number;
+  task: string;
+  completed: boolean;
+}
 
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTask, setNewTask] = useState("");
+
+  const addTask = () => {
+    if (newTask.trim() !== "") {
+      setTasks([
+        ...tasks,
+        { id: tasks.length, task: newTask.trim(), completed: false },
+      ]);
+      setNewTask("");
+    }
+  };
+
   return (
     <Container maxWidth="lg">
       <Box>
         <Typography variant="h3" component="h1" align="center">
           TODO
         </Typography>
-        <TextField label="task" />
-        <Button variant="contained">add</Button>
+        <TextField
+          label="今日やることを入力"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <Button variant="contained" onClick={addTask}>
+          add
+        </Button>
         <List>
-          <ListItem>
-            <Checkbox />
-            <ListItemText primary="Single-line item" />
-            <IconButton edge="end" aria-label="delete">
-              <DeleteIcon />
-            </IconButton>
-          </ListItem>
+          {tasks.map((task) => (
+            <ListItem>
+              <Checkbox checked={task.completed} />
+              <ListItemText primary={task.task} />
+              <IconButton edge="end" aria-label="delete">
+                <DeleteIcon />
+              </IconButton>
+            </ListItem>
+          ))}
         </List>
       </Box>
     </Container>
